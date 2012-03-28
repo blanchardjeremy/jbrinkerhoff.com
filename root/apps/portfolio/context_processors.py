d4dc5d2c3cd6e7df2234f8f8d2b1b8fd9a33b1a1
Jeremy Blanchard <auzigog@gmail.com>
2012-03-28 03:07:36 -0700

    Change the way home page is calculated

--- root/apps/portfolio/context_processors.py
+++ root/apps/portfolio/context_processors.py
@@ -1,5 +1,9 @@
+from django.core.urlresolvers import reverse
 from portfolio.models import Quote
 
 def quotes(request):
     quote = Quote.objects.all().order_by('?')[0]
     return {'quote': quote}
+
+def is_home(request):
+    return {'is_home': bool(request.META['PATH_INFO'] == reverse('home'))}
--- root/settings.py
+++ root/settings.py
@@ -75,6 +75,7 @@ TEMPLATE_CONTEXT_PROCESSORS = [
     'django.core.context_processors.request',
     'django.contrib.messages.context_processors.messages',
     'portfolio.context_processors.quotes',
+    'portfolio.context_processors.is_home',
 ]
 
 TEMPLATE_LOADERS = [
