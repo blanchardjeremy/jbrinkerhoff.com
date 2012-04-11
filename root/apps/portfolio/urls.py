df3afefed998a4ea1bd170517c29ef3e2dfbc6bf
Jeremy Blanchard <auzigog@gmail.com>
2012-04-11 08:47:27 -0700

    Add scrollspy, add hover and active colors to nav

--- root/apps/portfolio/urls.py
+++ root/apps/portfolio/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import *
 from portfolio.views import *
 
 urlpatterns = patterns('',
-    url(r'^inner/$',    InnerView.as_view(),  name='inner'),
+    url(r'^acorn/$',    InnerView.as_view(),  name='inner'),
     url(r'^$',          HomeView.as_view(),  name='home'),
 
 
